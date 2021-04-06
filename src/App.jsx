3f1bfd4e8f4962974631f976ce2309cba022f74d
mrvanagas <lukonelis1@gmail.com>
2021-04-06 08:48:15 +0300

    feat: onClick event hook

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   return (
     <div>
      <h1>{counter}</h1>
+     <div>
+       <button onClick={() => setCounter(counter +1)}>increase</button>
+       <button onClick={() => setCounter(counter -1)}>decrease</button>
+     </div>
     </div>
   );
 }
