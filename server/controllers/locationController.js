3bef3ed611afa3540873b4c4095a651331473519
mrvanagas <lukonelis1@gmail.com>
2021-04-22 10:52:54 +0300

    feat: location controller

--- server/controllers/locationController.js
+++ server/controllers/locationController.js
@@ -1,4 +1,5 @@
 const LocationModel = require('../models/LocationModel');
+const CourseModel = require('../models/CourseModel');
 const Mongoose = require('mongoose');
 const updateOptions = require('../config/updateOptions');
 
@@ -53,10 +54,32 @@ module.exports.deleteLocation = async (req, res) => {
   try {
     if (!Mongoose.Types.ObjectId.isValid(id))
       throw new Error('Location id is not valid');
-    const deletedLocation = await LocationModel.findByIdAndDelete(id);
-    if (deletedLocation === null)
+
+    const locationToDelete = await LocationModel.findById(id);
+    if (locationToDelete === null)
       throw new Error(`Location with id '${id}' not found.`);
-    res.status(200).json({ id });
+    // Surandame kursus, kurie turi trinamą lokaciją kaip priklausomybę
+    const coursesDependentOnLocation = await CourseModel.find({
+      locations: { $in: [id] }
+    });
+    // Surandame visus kursus, kurie turi trinamą lokaciją, kaip vienintelę lokaciją
+    const courseDependentOnOnlyLocationToDelete = coursesDependentOnLocation
+      .filter(({ locations }) => locations.length === 1);
+    // Jeigu yra kursų, kurie lokacijų masyve turi trinamają lokaciją, draudžiame trinimą
+    if (courseDependentOnOnlyLocationToDelete.length > 0) {
+      const coursesTitles = courseDependentOnOnlyLocationToDelete.map(({ title }) => title);
+      throw new Error(
+        `Location is a single dependency in locations: ${coursesTitles.join(', ')}.Please delete these courses first.`
+      );
+    }
+    // Jeigu nėra lokacijų, kurie turėtų trinamą lokaciją kaip vienintelę priklausomybę, pašaliname trinamą lokaciją iš kursų lokacijų masyvo
+    await CourseModel.updateMany(
+      { locations: { $in: [id] } }, // Pagal ką ieškome
+      { $pull: { locations: id } } // Ką keičiame
+    );
+    // Ištriname lokaciją
+    const deletedLocation = await LocationModel.findByIdAndDelete(id);
+    res.status(200).json(deletedLocation);
   }
   catch (error) {
     res.status(404).json({ message: error.message });
